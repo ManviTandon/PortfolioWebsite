c2a74dba398f91dad207fc570a36e8c965756879
Manvi <manvitandon@github.com>
2018-02-13 13:08:48 -0500

    stick figure

--- week1/script.js
+++ week1/script.js
@@ -1,17 +1,15 @@
 function setup() {
-	var mydiv = document.getElementById("sketch-holder");
 	var myCanvas = createCanvas(100, 100);
-	mydiv.addChild(myCanvas);
-	//myCanvas.parent('sketch-holder');
+	myCanvas.parent('sketch-holder');
 	background(255);
 	strokeWeight(4); 
 	fill(0);
 	
 	// Draws head
-	ellipse(50, 0, 20, 20);
+	ellipse(50, 12, 20, 20);
 
 	// Draws body
-	line(50, 20, 50, 50); 
+	line(50, 20, 50, 70); 
 
 	// Draws right arm 
 	line(50, 35, 90, 40); 
@@ -23,7 +21,7 @@ function setup() {
 	line(50, 70, 65, 100); 
 
 	//Draws left leg
-	line(50, 70, 65, 100); 
+	line(50, 70, 35, 100); 
 
 
 }
