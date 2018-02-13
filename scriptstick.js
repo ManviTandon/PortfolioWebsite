c0023236e790ce1660c42c5a121f2801016aa799
Manvi <manvitandon@github.com>
2018-02-13 13:15:15 -0500

    fix

--- scriptstick.js
+++ scriptstick.js
@@ -1,4 +1,4 @@
-function setup() {
+xfunction setup() {
 	var myCanvas = createCanvas(100, 100);
 	myCanvas.parent('sketch-holder');
 	background(255);
@@ -6,10 +6,10 @@ function setup() {
 	fill(0);
 	
 	// Draws head
-	ellipse(50, 0, 20, 20);
+	ellipse(50, 12, 20, 20);
 
 	// Draws body
-	line(50, 20, 50, 50); 
+	line(50, 20, 50, 70); 
 
 	// Draws right arm 
 	line(50, 35, 90, 40); 
@@ -21,7 +21,7 @@ function setup() {
 	line(50, 70, 65, 100); 
 
 	//Draws left leg
-	line(50, 70, 65, 100); 
+	line(50, 70, 35, 100); 
 
 
 }
