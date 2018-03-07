967182668be929d6e035507517a396786ecf163e
Manvi <manvitandon@github.com>
2018-03-06 21:07:42 -0500

    data

---
+++ week1/script7.js
@@ -0,0 +1,10 @@
+let p = 'potato';
+if (p == 'potato'){
+  text('yep', 5, 50);
+}
+
+//if p.equals("potato") 
+// use a backlash to include quotes in a string
+String[] lines = loadStrings("file.txt");
+println("There are " + lines.length + " lines.");
+printArray(lines);
